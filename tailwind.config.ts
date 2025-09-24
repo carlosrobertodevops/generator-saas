eb88b8af87f4df6327c599aca7e96ed1a65d4439
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-09-23 22:18:08 -0300

    Ajustes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/stories/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,4 +21,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
